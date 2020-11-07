4fb01c2d1c8b483309ab42e82e63aa571057cb03
GWAndrew <gwandrewgd@gmail.com>
2020-11-07 11:15:11 +0100

    bot update

--- bot.py
+++ bot.py
@@ -83,31 +83,34 @@ async def unban(ctx, id: int):
 @bot.command(pass_context=True)
 @has_permissions(kick_members=True)
 async def purge(ctx, user:discord.Member):
-    with open('users.json', 'r') as f:
-            users = json.load(f)
+    try:
+        with open('users.json', 'r') as f:
+                users = json.load(f)
 
-    role_to_add = discord.utils.get(ctx.guild.roles, name="💉 Purged")
-    a=0
-    users[str(user.id)]={}
-    removing_roles=[]
-    for role in user.roles:
-        if a==0:
-            a=1
-        else:
-            removing_roles.append(f"{role.id}")
-            role_id = int(role.id)
-            role_name = discord.utils.get(ctx.guild.roles, id=role_id)
-            role_to_remove = discord.utils.get(ctx.guild.roles, name=f"{role_name}")
-            await user.remove_roles(role_to_remove)
-            print(role_to_remove)
+        role_to_add = discord.utils.get(ctx.guild.roles, name="💉 Purged")
+        a=0
+        users[str(user.id)]={}
+        removing_roles=[]
+        for role in user.roles:
+            if a==0:
+                a=1
+            else:
+                removing_roles.append(f"{role.id}")
+                role_id = int(role.id)
+                role_name = discord.utils.get(ctx.guild.roles, id=role_id)
+                role_to_remove = discord.utils.get(ctx.guild.roles, name=f"{role_name}")
+                await user.remove_roles(role_to_remove)
+                print(role_to_remove)
 
-    users[str(user.id)]=removing_roles
+        users[str(user.id)]=removing_roles
 
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
 
-    await user.add_roles(role_to_add)
-    await ctx.send("Purged :smiling_imp:")
+        await user.add_roles(role_to_add)
+        await ctx.send("Purged :smiling_imp:")
+    except:
+        await ctx.send("You cannot purge a staff member :pensive:")
 
 
 
@@ -115,23 +118,26 @@ async def purge(ctx, user:discord.Member):
 @bot.command(pass_context=True)
 @has_permissions(kick_members=True)
 async def unpurge(ctx, user:discord.Member):
-    with open('users.json', 'r') as f:
-            users = json.load(f)
+    try:
+        with open('users.json', 'r') as f:
+                users = json.load(f)
 
-    for role in users[str(user.id)]:
-        role_id = int(role)
-        role_name = discord.utils.get(ctx.guild.roles, id=role_id)
-        role_to_add = discord.utils.get(ctx.guild.roles, name=f"{role_name}")
-        await user.add_roles(role_to_add)
-        print(role_to_add)
+        for role in users[str(user.id)]:
+            role_id = int(role)
+            role_name = discord.utils.get(ctx.guild.roles, id=role_id)
+            role_to_add = discord.utils.get(ctx.guild.roles, name=f"{role_name}")
+            await user.add_roles(role_to_add)
+            print(role_to_add)
 
-    role_to_remove = discord.utils.get(ctx.guild.roles, name="💉 Purged")
+        role_to_remove = discord.utils.get(ctx.guild.roles, name="💉 Purged")
 
-    await ctx.author.remove_roles(role_to_remove)
-    await ctx.send("Unpurged :smiling_imp:")
+        await ctx.author.remove_roles(role_to_remove)
+        await ctx.send("Unpurged :smiling_imp:")
 
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+    except:
+        await ctx.send("You cannot unpurge a staff member :pensive:")
 
 
 token = open("token.txt", "r")
