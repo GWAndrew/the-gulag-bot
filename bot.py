5d2614184a1cb7abc22e27beba70d31cfe98d581
GWAndrew <gwandrewgd@gmail.com>
2020-11-07 19:45:28 +0100

    bot update

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def unban(ctx, id: int):
 
 @bot.command(pass_context=True)
 @has_permissions(kick_members=True)
-async def mute(ctx, user:discord.Member, *, arg):
+async def mute(ctx, user:discord.Member, *, arg=None):
     #struser=str(user)
     #username=struser[:-5]
     if user.guild_permissions.kick_members:
@@ -113,47 +113,12 @@ async def mute(ctx, user:discord.Member, *, arg):
         await user.add_roles(role_to_add)
 
         embed=discord.Embed(color=0x940000)
-        embed.set_author(name=f"Muted {user} for {arg} 🔇")
+        if arg:
+            embed.set_author(name=f"Muted {user} for {arg} 🔇")
+        else :
+            embed.set_author(name=f"Muted {user} 🔇")
         await ctx.send(embed=embed)
 
-
-@mute.error
-@has_permissions(kick_members=True)
-async def mute_error(ctx, error):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        #struser=str(user)
-        #username=struser[:-5]
-        if user.guild_permissions.kick_members:
-            await ctx.send("You cannot mute a staff member :pensive:")
-        else:
-            with open('users.json', 'r') as f:
-                    users = json.load(f)
-
-            role_to_add = discord.utils.get(ctx.guild.roles, name="Muted")
-            a=0
-            users[str(user.id)]={}
-            removing_roles=[]
-            for role in user.roles:
-                if a==0:
-                    a=1
-                else:
-                    removing_roles.append(f"{role.id}")
-                    role_id = int(role.id)
-                    role_name = discord.utils.get(ctx.guild.roles, id=role_id)
-                    role_to_remove = discord.utils.get(ctx.guild.roles, name=f"{role_name}")
-                    await user.remove_roles(role_to_remove)
-                    print(role_to_remove)
-
-            users[str(user.id)]=removing_roles
-
-            with open('users.json', 'w') as f:
-                json.dump(users, f)
-
-            await user.add_roles(role_to_add)
-
-            embed=discord.Embed(color=0x940000)
-            embed.set_author(name=f"Muted {user} 🔇")
-            await ctx.send(embed=embed)
 
 
 
